27cf695b2b2a79a5e2fd1961ec41f3f3e3c813fe
Mahefa04 <hei.mahefa.2@gmail.com>
2026-08-13 15:47:00 +0300

    feat add TeachingAssignment entity

---
+++ src/main/java/school/hei/graduates/entity/TeachingAssignment.java
@@ -0,0 +1,38 @@
+package school.hei.graduates.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TeachingAssignment {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id")
+    private Course course;
+
+    @ManyToOne
+    @JoinColumn(name = "teacher_id")
+    private Teacher teacher;
+
+    @ManyToOne
+    @JoinColumn(name = "group_id")
+    private Group group;
+}
