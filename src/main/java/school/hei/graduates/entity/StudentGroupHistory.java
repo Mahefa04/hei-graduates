6c33e65a8bfa1603a55c6bfbae744e557f306efb
Mahefa04 <hei.mahefa.2@gmail.com>
2026-08-13 15:46:18 +0300

    feat: add StudentGroupHistory entity

---
+++ src/main/java/school/hei/graduates/entity/StudentGroupHistory.java
@@ -0,0 +1,39 @@
+package school.hei.graduates.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.Instant;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class StudentGroupHistory {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id")
+    private Student student;
+
+    @ManyToOne
+    @JoinColumn(name = "group_id")
+    private Group group;
+
+    private Instant startDate;
+
+    private Instant endDate;
+}
