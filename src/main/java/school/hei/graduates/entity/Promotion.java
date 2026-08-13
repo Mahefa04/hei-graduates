de319e3aad0d24480274f8c3b6342c900868dd86
Mahefa04 <hei.mahefa.2@gmail.com>
2026-08-13 15:33:05 +0300

    feat: add Promotion entity

---
+++ src/main/java/school/hei/graduates/entity/Promotion.java
@@ -0,0 +1,28 @@
+package school.hei.graduates.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Promotion {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String ref;
+
+    private int startYear;
+}
