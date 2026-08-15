4c979724b95ba69104bc9308b0e54a4839519859
Mahefa04 <hei.mahefa.2@gmail.com>
2026-08-15 19:39:45 +0300

    fix: update Exam Repository

--- src/main/java/school/hei/graduates/repository/ExamRepository.java
+++ src/main/java/school/hei/graduates/repository/ExamRepository.java
@@ -9,5 +9,5 @@ import school.hei.graduates.entity.Exam;
 @Repository
 public interface ExamRepository extends JpaRepository<Exam, UUID> {
 
-    List<Exam> findByCourse_Id(UUID courseId);
+    List<Exam> findByCourseOffering_Id(UUID courseOfferingId);
 }
