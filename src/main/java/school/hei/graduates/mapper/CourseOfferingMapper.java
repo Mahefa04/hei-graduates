bc771d0baafff8a5c290fa143bc0dac47c134d09
Mahefa04 <hei.mahefa.2@gmail.com>
2026-08-15 19:11:45 +0300

    feat: add CourseOffering Mapper

---
+++ src/main/java/school/hei/graduates/mapper/CourseOfferingMapper.java
@@ -0,0 +1,39 @@
+package school.hei.graduates.mapper;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+import school.hei.graduates.endpoint.rest.model.CourseOfferingResponse;
+import school.hei.graduates.endpoint.rest.model.UpsertCourseOffering;
+import school.hei.graduates.entity.Course;
+import school.hei.graduates.entity.CourseOffering;
+import school.hei.graduates.entity.Group;
+
+@Component
+@AllArgsConstructor
+public class CourseOfferingMapper {
+
+    private final CourseMapper courseMapper;
+    private final GroupMapper groupMapper;
+
+    public CourseOffering toEntity(
+            UpsertCourseOffering request,
+            Course course,
+            Group group) {
+
+        return new CourseOffering(
+                request.id(),
+                course,
+                group,
+                request.semester(),
+                request.academicYear());
+    }
+
+    public CourseOfferingResponse toResponse(CourseOffering courseOffering) {
+        return new CourseOfferingResponse(
+                courseOffering.getId(),
+                courseMapper.toResponse(courseOffering.getCourse()),
+                groupMapper.toResponse(courseOffering.getGroup()),
+                courseOffering.getSemester(),
+                courseOffering.getAcademicYear());
+    }
+}
