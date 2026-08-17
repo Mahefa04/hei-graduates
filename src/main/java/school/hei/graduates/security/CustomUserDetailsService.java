bca082a34643c1ec3f44e2263d818f54aa2565c2
Mahefa04 <hei.mahefa.2@gmail.com>
2026-08-17 10:56:12 +0300

    feat: add CustomUserDetails service

---
+++ src/main/java/school/hei/graduates/security/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package school.hei.graduates.security;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import school.hei.graduates.entity.User;
+import school.hei.graduates.repository.UserRepository;
+
+@Service
+@AllArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+
+        User user =
+                userRepository
+                        .findByEmail(email)
+                        .orElseThrow(
+                                () -> new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .roles(user.getRole().name())
+                .build();
+    }
+}
