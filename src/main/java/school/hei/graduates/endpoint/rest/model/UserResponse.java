876ee3819af89169e1baaafa689d6c3136839bea
Mahefa04 <hei.mahefa.2@gmail.com>
2026-08-17 10:03:32 +0300

    feat: add User DTO

---
+++ src/main/java/school/hei/graduates/endpoint/rest/model/UserResponse.java
@@ -0,0 +1,11 @@
+package school.hei.graduates.endpoint.rest.model;
+
+import java.util.UUID;
+import school.hei.graduates.entity.Role;
+
+public record UserResponse(
+        UUID id,
+        String email,
+        Role role,
+        UUID studentId,
+        UUID teacherId) {}
