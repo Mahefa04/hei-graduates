60fa9b23448d577e69802561c110edef2e2da82c
Mahefa04 <hei.mahefa.2@gmail.com>
2026-08-18 17:49:28 +0300

    fix: rename class YearResultResponse to AcademicYearResultResponse

--- src/main/java/school/hei/graduates/endpoint/rest/model/AcademicYearResultResponse.java
+++ src/main/java/school/hei/graduates/endpoint/rest/model/AcademicYearResultResponse.java
@@ -4,7 +4,7 @@ import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
 
-public record YearResultResponse(
+public record AcademicYearResultResponse(
         UUID studentId,
         int year,
         BigDecimal average,
